a448317315d3d3201354815bb39a7fe38d84089d
carloschinga <chinga_carlos@hotmail.com>
2025-08-28 09:55:02 -0500

    Auth Estable ok

---
+++ src/main/java/com/ejemplo/jwtlogin/util/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.ejemplo.jwtlogin.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and()
+                .csrf().disable() // importante para POST sin CSRF token
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/auth/loginByUsername", "/auth/loginBartolito",
+                        "/auth/loginBartolitoByUsername",
+                        "/auth/loginInventarioByUsername", "/auth/loginInventario",
+                        "/auth/getUser",
+                        "/auth/getUserBartolito",
+                        "/auth/getUserInventario",
+                        "/menu/paginasxgrupo")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
